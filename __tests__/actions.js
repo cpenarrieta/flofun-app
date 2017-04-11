ed24715efe688aa08a1b6fe3ce3474047fa175eb
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2017-04-11 01:14:24 -0700

    add failing test for testing purpose

--- __tests__/actions.js
+++ __tests__/actions.js
@@ -7,9 +7,9 @@ describe('actions', () => {
     const expectedAction = {
       type: FETCH_FLOWERS,
       payload: [
-        { title: 'flower 1', image: 'img1', price: 30 },
-        { title: 'flower 2', image: 'img2', price: 40 },
-        { title: 'flower 3', image: 'img3', price: 50 },
+        { title: 'flower ', image: 'img1', price: 30 },
+        { title: 'flower ', image: 'img2', price: 40 },
+        { title: 'flower ', image: 'img3', price: 50 },
       ],
     }
 
