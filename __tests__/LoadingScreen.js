b42fda1f5493b3894d2f860aeb397608784ad24a
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2017-04-10 20:00:42 -0700

    add loading screen tests

---
+++ __tests__/LoadingScreen.js
@@ -0,0 +1,20 @@
+import 'react-native'
+import React from 'react'
+import renderer from 'react-test-renderer'
+import EStyleSheet from 'react-native-extended-stylesheet'
+import Colors from '../constants/colors'
+import { LoadingScreen } from '../src/commons'
+
+beforeAll(() => { // eslint-disable-line
+  EStyleSheet.build(Colors)
+});
+
+describe('LoadingScreen', () => {
+  test('renders correctly', () => {
+    const loading = renderer.create(
+      <LoadingScreen />
+    ).toJSON()
+
+    expect(loading).toMatchSnapshot()
+  })
+})
