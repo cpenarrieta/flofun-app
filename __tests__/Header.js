37af7fcd074349ffa26f96b43e0c96d5bf0b6946
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2017-04-10 20:21:59 -0700

    add Header tests

---
+++ __tests__/Header.js
@@ -0,0 +1,20 @@
+import 'react-native'
+import React from 'react'
+import renderer from 'react-test-renderer'
+import EStyleSheet from 'react-native-extended-stylesheet'
+import Colors from '../constants/colors'
+import { Header } from '../src/commons'
+
+beforeAll(() => { // eslint-disable-line
+  EStyleSheet.build(Colors)
+});
+
+describe('Header', () => {
+  test('renders correctly', () => {
+    const loading = renderer.create(
+      <Header title='foo test' />
+    ).toJSON()
+
+    expect(loading).toMatchSnapshot()
+  })
+})
