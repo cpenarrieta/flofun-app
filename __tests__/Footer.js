182a4c1480e057e4843f11255bfb41fe0f8d9aec
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2017-05-16 21:46:18 -0700

    add footer test

---
+++ __tests__/Footer.js
@@ -0,0 +1,21 @@
+import 'react-native'
+import React from 'react'
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import EStyleSheet from 'react-native-extended-stylesheet'
+import Colors from '../constants/colors'
+import Footer from '../src/commons/Footer'
+
+beforeAll(() => {
+  EStyleSheet.build(Colors)
+})
+
+describe('Footer', () => {
+  test('renders correctly', () => {
+    const footer = shallow(
+      <Footer />
+    )
+
+    expect(toJson(footer)).toMatchSnapshot()
+  })
+})
